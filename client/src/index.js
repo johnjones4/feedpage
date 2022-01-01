8e97511cd8cc46fec67bea9cb6decc3ae90df18e
John Jones <john@johnjonesfour.com>
2022-01-01 14:27:47 -0500

    new theme

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Reader from './Reader';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootswatch.css'
 
 ReactDOM.render(
   <React.StrictMode>
