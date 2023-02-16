048538b19d7da2d1a5330958576c93946300b6b8
John Jones <john@johnjonesfour.com>
2023-02-15 20:33:20 -0500

    ui change

--- Makefile
+++ Makefile
@@ -2,22 +2,12 @@ PROJECT=$(shell basename $(shell pwd))
 TAG=ghcr.io/johnjones4/${PROJECT}
 VERSION=$(shell date +%s)
 
-.PHONY: ui
-
 info:
 	echo ${PROJECT} ${VERSION}
 
 container:
-	docker build -t ${TAG} ./server
+	docker build -t ${TAG} ./
 	docker push ${TAG}:latest
 	docker image rm ${TAG}:latest
 
-ui:
-	cd client && npm install
-	cd client && npm run build
-	tar zcvf ui.tar.gz ./client/build
-	git tag ${VERSION}
-	git push origin ${VERSION}
-	gh release create ${VERSION} ui.tar.gz --generate-notes
-
-ci: container ui
+ci: container
--- server.go
+++ server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	_ "embed"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"main/feedpage"
 	"net/http"
 	"os"
 )
+
+//go:embed index.html
+var templateStr string
 
 var store feedpage.CurrentFeeds
 
@@ -28,12 +33,27 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, string(info))
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.New("t").Parse(templateStr)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+
+	store.Mux.Lock()
+	temp.Execute(w, &store)
+	store.Mux.Unlock()
+}
+
 func main() {
 	store = feedpage.CurrentFeeds{
 		Sections: make([]feedpage.FeedSection, 0),
 		Failures: &feedpage.FailureBuffer{},
 	}
 	go feedpage.FeedEngine(os.Getenv("OPML_URL"), &store)
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/api/feed", feedHandler)
 	http.HandleFunc("/api/status", statusHandler)
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
